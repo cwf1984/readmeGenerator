824bf08944be0ac16096bf71ed0bcc66f1b9c614
Vanessa Malloy <catwoman.fan.84@gmail.com>
2020-11-28 18:50:58 -0800

    questions, beginning functions, generateMarkdown code

--- index.js
+++ index.js
@@ -1,16 +1,71 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
 // array of questions for user
 const questions = [
 
+    {
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Please provide a description of your project.',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'How is your project installed?',
+      name: 'installation',
+    },
+    {//license
+      type: 'list',
+      message: 'What type of license are you using?',
+      name: 'license',
+      choices: ['Apache', 'Boost Software License', 'GNU (General Public License)', 'MIT', 'Mozilla', 'The Unlicense']
+    },
+    {
+      type: 'input',
+      message: 'Has anyone contributed to your project? If so, please include names.',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'How should users test your project?',
+      name: 'test',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your github user name.',
+      name: 'username',
+    },
+        {
+      type: 'input',
+      message: 'Please enter your email address.',
+      name: 'email',
+    },
+
 ];
 
+  //create new function () that is called above 
+
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(filename, data) {
+    fs.writeFile('README.md', data, (err) =>
+    err ? console.log(err) : console.log('Data Logged')
+    
+    )};
 
 // function to initialize program
 function init() {
 
-}
+    inquirer.prompt(questions).then((response) =>{
+        JSON.stringify(data)
+})
+
+};
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Table of Contents
+  1. Description
+  2. Installation Instructions
+  3. Usage Information
+  4. Contribution Guidelines
+  5. Test
+  6. Questions
+
+  ## Description
+  ${data.description}
+
+  ## Installation Instructions
+  ${data.installation}
+
+  ## Usage Information
+  ${data.usage}
+
+  ## Contribution Guidelines
+  ${data.contribution}
+
+  ## Test
+  ${data.test}
+
+  ## Questions
+  For additional questions or information, please reach me at the links below:
+  
+  Github: [${data.username}]
+  Email: [${data.email}]
 
 `;
 }
